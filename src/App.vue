01695f6b93c454db12fa2670c3a29e8393f54f6a
Andreseag <castrogomezand@gmail.com>
2025-01-18 22:02:06 -0500

    feat: add splash screen to app :sparkles:

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,47 @@
 <template>
-
-  <Home />
-  
+  <Suspense>
+    <template #default>
+      <Home />
+    </template>
+    <template #fallback>
+      <SplashScreen />
+    </template>
+  </Suspense>
 </template>
 
 <script>
 
-import Home from "@/components/Home.vue";
+import { defineAsyncComponent } from "vue";
+
+// Components
+import SplashScreen from "@/components/SplashScreen.vue";
+
 
 export default {
   components: {
-    Home
-  }
+    SplashScreen,
+    Home: defineAsyncComponent(() => 
+      new Promise((resolve) => {
+        setTimeout(() => {
+          resolve(import("@/components/Home.vue"));
+        }, 3000);
+      })
+    ),}
 }
 
 </script>
 
+
 <style>
+html,
+body,
+.app {
+  min-height: 100vh;
+  margin: 0;
+  font-family: Arial, Helvetica, sans-serif;
+}
+* {
+  --brand-green: #04b500;
+  --brand-blue: #0689b0;
+}
 </style>

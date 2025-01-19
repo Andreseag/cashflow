1609488496d0d52ebe5cefd6ecd73563c67d32ff
Andreseag <castrogomezand@gmail.com>
2025-01-19 17:10:04 -0500

    feat: add movements and resume components :sparkles:

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,19 +3,30 @@
     <template #header>
       <Header></Header>
     </template>
+    <template #resume>
+      <Resume></Resume>
+    </template>
+    <template #movements>
+      <Movements></Movements>
+    </template>
   </Layout>
 
 </template>
 
 <script>
 
+// Components Imports
 import Layout from "@/components/Layout.vue";
 import Header from "@/components/Header.vue";
+import Resume from "@/components/Resume/index.vue";
+import Movements from "@/components/Movements/index.vue";
 
 export default {
   components: {
     Layout,
     Header,
+    Resume,
+    Movements,
   },
 };
 
